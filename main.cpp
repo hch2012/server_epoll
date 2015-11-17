277cd3d8b05d1fa952579bd137c1e9e3c910aef5
胡程皓 <hch@goujiawang.com>
2015-11-17 16:31:25 +0800

    epoll_test

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ void testEpoll(){
     struct epoll_event ev,events[5];//ev用于注册事件，数组用于返回要处理的事件
     epfd=epoll_create(1);//只需要监听一个描述符——标准输入
     ev.data.fd=STDIN_FILENO;
-    ev.events=EPOLLIN|EPOLLET;//监听读状态同时设置ET模式
+    ev.events=EPOLLIN;//监听读状态同时设置ET模式
     epoll_ctl(epfd,EPOLL_CTL_ADD,STDIN_FILENO,&ev);//注册epoll事件
     for(;;)
    	{
