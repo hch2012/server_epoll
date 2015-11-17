e64b9e778339dbf3d720a93b97e2ed0bc0b0848f
胡程皓 <hch@goujiawang.com>
2015-11-17 16:24:28 +0800

    epoll_test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "thread_pool.h"
 #include "one_task.h"
 #include <sstream>
+#include <sys/epoll.h>
 using namespace std;
 SqlPool* pool=NULL;
 void testThreadPool(int argc, char const *argv[]);
@@ -22,10 +23,11 @@ void* insert(void* args){
 }
 int main(int argc, char const *argv[])
 {	
-	testThreadPool(argc,argv);
+	testEpoll();
+	//testThreadPool(argc,argv);
 }
 void testThreadPool(int argc, char const *argv[]){
-	ThreadPool* pool=new ThreadPool(100);
+	ThreadPool* pool=new ThreadPool(1000);
 	string str="task ";
 	ostringstream stream;
 	for(int i=0;i<100000;i++){
@@ -58,3 +60,21 @@ void testSqlPool(int argc, char const *argv[]){
 	}
 	cout<<pool->node_size()<<endl;
 }
+void testEpoll(){
+    int epfd,nfds;
+    struct epoll_event ev,events[5];//ev用于注册事件，数组用于返回要处理的事件
+    epfd=epoll_create(1);//只需要监听一个描述符——标准输入
+    ev.data.fd=STDIN_FILENO;
+    ev.events=EPOLLIN|EPOLLET;//监听读状态同时设置ET模式
+    epoll_ctl(epfd,EPOLL_CTL_ADD,STDIN_FILENO,&ev);//注册epoll事件
+    for(;;)
+   	{
+	     nfds=epoll_wait(epfd,events,5,-1);
+	     for(int i=0;i<nfds;i++)
+	     {
+	        if(events[i].data.fd==STDIN_FILENO)
+	           cout<<"hello world!"<<endl;
+	     }
+   	}
+}
+
