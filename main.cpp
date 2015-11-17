a1128d8011e7b99e9175401715af1ac267cb411c
胡程皓 <hch@goujiawang.com>
2015-11-17 16:42:10 +0800

    epoll_test

--- main.cpp
+++ main.cpp
@@ -63,24 +63,19 @@ void testSqlPool(int argc, char const *argv[]){
 }
 void testEpoll(){
     int epfd,nfds;
-    char buf[5];
     struct epoll_event ev,events[5];//ev用于注册事件，数组用于返回要处理的事件
-    epfd=epoll_create(1);//只需要监听一个描述符——标准输入
-    ev.data.fd=STDIN_FILENO;
-    ev.events=EPOLLIN;//监听读状态同时设置ET模式
-    epoll_ctl(epfd,EPOLL_CTL_ADD,STDIN_FILENO,&ev);//注册epoll事件
+    epfd=epoll_create(1);//只需要监听一个描述符——标准输出
+    ev.data.fd=STDOUT_FILENO;
+    ev.events=EPOLLOUT|EPOLLET;//监听读状态同时设置ET模式
+    epoll_ctl(epfd,EPOLL_CTL_ADD,STDOUT_FILENO,&ev);//注册epoll事件
     for(;;)
-   	{
-	     nfds=epoll_wait(epfd,events,5,-1);
-	     for(int i=0;i<nfds;i++)
-	     {
-	        if(events[i].data.fd==STDIN_FILENO){
-	        	int c=read(events[i].data.fd,buf,5);
-	        	if(buf[c-1]=='\n')
-	        		buf[c-1]='\0';
-	           cout<<buf<<endl;
-	        }
-	     }
-   	}
+   {
+      nfds=epoll_wait(epfd,events,5,-1);
+      for(int i=0;i<nfds;i++)
+     {
+         if(events[i].data.fd==STDOUT_FILENO)
+             cout<<"hello world!"<<endl;
+     }
+   }
 }
 
